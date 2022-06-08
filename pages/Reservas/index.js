7a06d6b63b309dae6077e802c605fb6472093695
diglean <diegoleandro2002@gmail.com>
2022-06-08 13:39:09 -0300

    actions.js

--- pages/Reservas/index.js
+++ pages/Reservas/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { removeReserve } from '../../store/modules/reserve/actions';
 import { MdDelete } from 'react-icons/md';
 import './style.css';
 
@@ -7,39 +8,36 @@ export default function Reservas() {
   const dispatch = useDispatch();
   const reserves = useSelector(state => state.reserve)
 
-  function handleRemove(id){
-    dispatch({
-      type: 'REMOVE_RESERVE',
-      id,
-    })
+  function handleRemove(id) {
+    dispatch(removeReserve(id));
   }
 
- return (
-   <div>
-    <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
+  return (
+    <div>
+      <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
 
-    {reserves.map(reserve =>(
-    <div className="reservas" key={reserve.id}>
-      <img
-      src={reserve.image}
-      alt={reserve.title}
-      />
-      <strong>{reserve.title}</strong>
-      <span>Quantidade: {reserve.amount}</span>
-      <button
-      type="button"
-      onClick={()=> handleRemove(reserve.id)}
-      >
-        <MdDelete size={20} color="#191919" />
-      </button>
+      {reserves.map(reserve => (
+        <div className="reservas" key={reserve.id}>
+          <img
+            src={reserve.image}
+            alt={reserve.title}
+          />
+          <strong>{reserve.title}</strong>
+          <span>Quantidade: {reserve.amount}</span>
+          <button
+            type="button"
+            onClick={() => handleRemove(reserve.id)}
+          >
+            <MdDelete size={20} color="#191919" />
+          </button>
+        </div>
+
+      ))}
+
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
+
     </div>
-      
-    ))}
-
-    <footer>
-      <button type="button">Solicitar Reservas</button>
-    </footer>
-
-   </div>
- );
+  );
 }
